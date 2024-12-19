655d7dd35018f6ef806a8427bc1bd7e1de21746e
PakoMtzR <francisco.2k1@gmail.com>
2024-12-19 16:02:00 -0600

    control del jugador con parpadeo y efectos de sonidos

--- game.py
+++ game.py
@@ -1,6 +1,40 @@
 import pygame   # Libreria para el desarrollo de videojuegos
 import os       # Libreria para manejo de archivos
-import random
+import random   # Libreria para el manejo de numeros aleatorios
+
+import cv2              # Libreria para el manejo de imagenes
+import mediapipe as mp  # Libreria para la deteccion del parpadeo
+
+# Configuracion de mediapipe
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(refine_landmarks=True)
+
+# Inicializamos la camara
+cap = cv2.VideoCapture(0)  
+
+# Función para detectar el parpadeo
+def detect_blink():
+    ret, frame = cap.read()
+    if not ret:
+        return False
+    
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = face_mesh.process(frame)
+
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            # Puntos de referencia para los ojos
+            left_eye = [face_landmarks.landmark[i] for i in [145, 159]]
+            right_eye = [face_landmarks.landmark[i] for i in [374, 386]]
+
+            # Calcular distancias
+            left_eye_dist = abs(left_eye[0].y - left_eye[1].y)
+            right_eye_dist = abs(right_eye[0].y - right_eye[1].y)
+
+            # Detectar parpadeo si las distancias son pequeñas
+            if left_eye_dist < 0.02 and right_eye_dist < 0.02:
+                return True
+    return False
 
 # Inicializamos pygame
 pygame.init()
@@ -25,6 +59,7 @@ GRAY = (100,100,100)
 # Fuente
 font = pygame.font.Font(None, 28)
 
+# Clase dinosaurio (jugador)
 class Dino:
     def __init__(self):
         self.x = 70
@@ -41,6 +76,9 @@ class Dino:
         ]
         self.collision_image = pygame.image.load(os.path.join("my-assets\dino", "dino_3.png"))
 
+        # Cargar sonido de salto del dinosaurio
+        self.jump_sound = pygame.mixer.Sound(os.path.join("my-assets\sounds", "jump-sound.mp3"))
+
         self.current_frame = 0
         self.animation_time = 0
 
@@ -49,8 +87,9 @@ class Dino:
         
     def jump(self):
         if not self.jumping:
-            self.velocity = -10
             self.jumping = True
+            self.velocity = -10
+            self.jump_sound.play()
     
     def move(self):
         self.velocity += self.gravity
@@ -66,6 +105,7 @@ class Dino:
                 self.current_frame = (self.current_frame + 1) % len(self.running_images)
 
     def draw(self, collision):
+        # Cambiamos imagen dependiendo el estado del jugador
         if collision:
             self.image = self.collision_image
         elif not self.jumping:
@@ -73,8 +113,10 @@ class Dino:
         else:
             self.image = self.jump_image
 
+        # Dibujamos el dinosaurio en la panatalla
         screen.blit(self.image, (self.x, self.y))
 
+# Clase para los objetos dentro del juego
 class Object:
     def __init__(self, x, y, image):
         self.x = x
@@ -111,15 +153,25 @@ class Cloud(Object):
     
 class Game:
     def __init__(self):
+        self.running_game = True
+        self.blink_control_enable = False
+        
         self.player = Dino()
+        self.collision = False
         self.velocity = 4
         self.cactus = Cactus()
         self.score = 0
-        self.collision = False
-        self.running_game = True
         self.count = 1
+
+        # Creamos las nubes del escenario
         self.clouds = [Cloud() for i in range(6)]
- 
+
+        # Sonidos del juego
+        self.new_level_sound = self.die_sound = pygame.mixer.Sound(os.path.join("my-assets\sounds", "point-sound.mp3"))
+        self.collision_sound = pygame.mixer.Sound(os.path.join("my-assets\sounds", "pacman-dies.mp3"))
+
+    # Funcion para manejar los eventos dentro del juego
+    # cierre del juego, teclado, parpadeo, etc.
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -130,7 +182,15 @@ class Game:
                         self.restart()
                     else:
                         self.player.jump()
-    
+        
+        # Control de saltos detectando el parpadeo usando mediapipe
+        if self.blink_control_enable and detect_blink():
+            if self.collision:
+                self.restart()
+            else:
+                self.player.jump()
+
+    # Actualiza el estado de los elementos del juego
     def update(self):
         if self.collision:
             return
@@ -148,13 +208,16 @@ class Game:
         # Detectar colisiones
         if (self.player.x < self.cactus.x + self.cactus.image.get_width()) and (self.player.x + self.player.image.get_width() > self.cactus.x) and (self.player.y < self.cactus.y + self.cactus.image.get_height()) and (self.player.y + self.player.image.get_height() > self.cactus.y):
             self.collision = True
+            self.collision_sound.play()
 
         # Incrementar puntuacion
         self.score += 1
-        if self.score == self.count*2*FPS:
+        if self.score == self.count*4*FPS:
+            self.new_level_sound.play()
             self.count += 1
             self.velocity += 1
 
+    # Dibuja todos los elementos visibles del juego
     def draw(self):
         # Dibujar escenario
         screen.fill(WHITE)
@@ -176,6 +239,8 @@ class Game:
         if self.collision:
             message = font.render("Press SPACE to restart", True, BLACK)
             screen.blit(message, (WIDTH // 2 - message.get_width() // 2, HEIGTH // 2))
+
+        # Actualiza la pantalla
         pygame.display.update()
     
     def restart(self):
@@ -196,4 +261,8 @@ class Game:
 # Ejecutar el juego
 game = Game()
 game.run()
+
+# Libera los recursos cuando se cierre el juego
+cap.release()
+cv2.destroyAllWindows()
 pygame.quit()
